0d47c741c65e86d3cffedba3b65ebe4064f2a473
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-05 19:41:22 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,7 @@
-describe "./calculator.rb" do
-  it "" do 
-    first_number = 6
-end
+# Add your variables here
+first_number = 5
+second_number = 6
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
